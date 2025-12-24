2563f7cc736a9bffb9040f4270ff8f3175fd6944
Syronss <143046056+Syronss@users.noreply.github.com>
2025-12-24 05:43:56 +0300

    Enable formatter and autocomplete providers
    Uncommented formatter and autocomplete registration functions.

--- src/providers/init.ts
+++ src/providers/init.ts
@@ -1,18 +1,20 @@
-// import { formatter, rangeFormatter } from "./format/handleFormat";
-// the auto complete isn't implemented yet.
-import {autoCompleteProvider} from "./autocomplete/autocompleteProvider";
+// Yorum satırlarını kaldırdık:
+import { formatter, rangeFormatter } from "./format/handleFormat"; 
+import { autoCompleteProvider } from "./autocomplete/autocompleteProvider";
 import * as monaco from "monaco-editor";
 
 export function initFormatter() {
-	// monaco.languages.registerDocumentFormattingEditProvider("luau", formatter);
-	// monaco.languages.registerDocumentRangeFormattingEditProvider(
-	// 	"luau",
-	// 	rangeFormatter
-	// );
+    // Aşağıdaki satırların başındaki // işaretlerini kaldırdık:
+    monaco.languages.registerDocumentFormattingEditProvider("luau", formatter);
+    monaco.languages.registerDocumentRangeFormattingEditProvider("luau", rangeFormatter);
 }
 
 export function initAutocomplete() {
-	monaco.languages.registerCompletionItemProvider("luau", autoCompleteProvider);
+    monaco.languages.registerCompletionItemProvider("luau", autoCompleteProvider);
 }
 
-export default initAutocomplete;
+// init fonksiyonunu dışarıdan çağırmak için birleştirici bir fonksiyon ekleyelim
+export default function initProviders() {
+    initFormatter();
+    initAutocomplete();
+}
